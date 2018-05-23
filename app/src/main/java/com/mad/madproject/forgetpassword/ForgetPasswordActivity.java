bb39b1c098bbc6fe2d36d87e9ed54e5932c9d64d
Limyandi Vicotrico <limyandi@gmail.com>
2018-05-24 01:13:50 +1000

    ViewModel Implementation

--- app/src/main/java/com/mad/madproject/forgetpassword/ForgetPasswordActivity.java
+++ app/src/main/java/com/mad/madproject/forgetpassword/ForgetPasswordActivity.java
@@ -1,20 +1,23 @@
 package com.mad.madproject.forgetpassword;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.mad.madproject.R;
+import com.mad.madproject.databinding.ActivityForgetPasswordBinding;
 import com.mad.madproject.login.LoginActivity;
 import com.mad.madproject.utils.Utils;
 
@@ -22,78 +25,39 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class ForgetPasswordActivity extends AppCompatActivity implements ForgetPasswordView {
+public class ForgetPasswordActivity extends AppCompatActivity implements ForgetPasswordViewModel.ViewListener {
 
-    @BindView(R.id.email)
-    EditText inputEmail;
-    @BindView(R.id.btn_reset_password)
-    Button btnReset;
-    @BindView(R.id.btn_back)
-    Button btnBack;
-    @BindView(R.id.progressBar)
-    ProgressBar progressBar;
-
-    private FirebaseAuth auth;
-    private ForgetPasswordPresenter mForgetPasswordPresenter;
+    ForgetPasswordViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget_password);
-
         //create the model using the view model provider .of (classname)
-
         //observe(livedata fields from the viewmodel)
 
-        //bind butterknife after setContentView.
-        ButterKnife.bind(this);
+        ActivityForgetPasswordBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_forget_password
+        );
 
-        auth = FirebaseAuth.getInstance();
-        mForgetPasswordPresenter = new ForgetPasswordPresenterImpl(auth);
+        viewModel = new ForgetPasswordViewModel();
 
-        mForgetPasswordPresenter.attachView(this);
-    }
-
-    @OnClick(R.id.btn_reset_password) void onResetButtonClick() {
-        String emailText = inputEmail.getText().toString().trim();
-        mForgetPasswordPresenter.requestForgetPassword(emailText);
-    }
-
-    @OnClick(R.id.btn_back) void onBackButtonClicked() {
-        finish();
+        viewModel.setViewListener(this);
+        binding.setViewModel(viewModel);
     }
 
     @Override
-    public Context getContext() {
-        return this;
-    }
-
-    @Override
-    public void showValidationError(String message) {
-        Utils.showMessage(this, message);
-    }
-
-    @Override
-    public void forgetPasswordSuccess() {
-        //TODO: change this, we should not use toast later.
-        Utils.showMessage(this, "Forget Password Success! Check your email!");
+    public void onEmailSentSuccess() {
         Utils.setIntent(this, LoginActivity.class);
     }
 
     @Override
-    public void forgetPasswordError() {
-        Utils.showMessage(this, "Forget password error!");
+    public void onBackSuccess() {
+        Utils.setIntent(this, LoginActivity.class);
     }
 
     @Override
-    public void setProgressVisibility(boolean visibility) {
-        if (visibility) progressBar.setVisibility(View.VISIBLE);
-        else progressBar.setVisibility(View.INVISIBLE);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mForgetPasswordPresenter.detachView();
+    public void onMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppTheme);
+        builder.setTitle(title).setMessage(message).create().show();
     }
 }
