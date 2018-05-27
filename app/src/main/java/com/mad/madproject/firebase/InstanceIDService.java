bedbe21397fa390fe98f9c273b34b8a2be8c1030
Limyandi Vicotrico <limyandi@gmail.com>
2018-05-27 22:07:33 +1000

    First Version FPN

---
+++ app/src/main/java/com/mad/madproject/firebase/InstanceIDService.java
@@ -0,0 +1,37 @@
+package com.mad.madproject.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by limyandivicotrico on 5/27/18.
+ */
+
+public class InstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "InstanceIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //for later development because currently we dont need to send any token to server.
+    }
+}
