ac9224c0ffd12b2f4d517d0536d66008d2558c17
Limyandi Vicotrico <limyandi@gmail.com>
2018-06-05 11:15:36 +1000

    Child Fragment for fitlering in my trips

---
+++ app/src/main/java/com/mad/madproject/fragments/FilterTripFragment.java
@@ -0,0 +1,140 @@
+package com.mad.madproject.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.mad.madproject.R;
+import com.mad.madproject.adapter.ItineraryPreviewAdapter;
+import com.mad.madproject.model.ItineraryPreview;
+import com.mad.madproject.utils.Constant;
+import com.mad.madproject.utils.Util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class FilterTripFragment extends Fragment {
+
+    private ArrayList<ItineraryPreview> mItineraryPreviewList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private ItineraryPreviewAdapter mItineraryPreviewAdapter;
+    private TextView mNoTripTextView;
+
+    private static final String FILTER_TYPE = "FILTER_TYPE";
+    private static final String POSITION = "POSITION";
+
+    public FilterTripFragment() {
+
+    }
+
+    public static FilterTripFragment newInstance(int position) {
+        FilterTripFragment fragment = new FilterTripFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt(POSITION, position);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_filter_itinerary, container, false);
+
+        mNoTripTextView = (TextView) rootView.findViewById(R.id.text_no_itinerary);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.filter_itinerary_rv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+
+
+        //setup the filter with int.
+        int position = 0;
+        if (getArguments() != null) {
+            position = getArguments().getInt(POSITION);
+        }
+
+        //setup the  data.
+        getItineraryPreviewDataFiltered(rootView, position);
+
+        return rootView;
+    }
+
+
+    //TODO: Can be better, we call this everytime we navigate to another page.
+    /**
+     * Call database to get the data for the itinerary preview that is already filtered.
+     * @param rootView refer to the view to send the database file to.
+     * @param position refer to the filter type and position
+     */
+    private void getItineraryPreviewDataFiltered(final View rootView, final int position) {
+
+        Util.getDatabaseReference("ItineraryPreview").orderByChild("ownerId").equalTo(Util.getUserUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mItineraryPreviewList.clear();
+                for(DataSnapshot itineraryView: dataSnapshot.getChildren()) {
+                    Log.d(Constant.LOG_TAG, itineraryView.toString());
+                    ItineraryPreview crawledView = itineraryView.getValue(ItineraryPreview.class);
+                    Date endDate = new Date();
+                    Date todayDate = new Date();
+
+                    try {
+                        Log.d("Try", crawledView.getEndDate());
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                        endDate = formatter.parse(crawledView.getEndDate());
+                    } catch (ParseException e) {
+                        Log.d("Try", "Failed");
+                        e.printStackTrace();
+                    }
+
+                    //if position == 0 ("All")
+                    if(position == 0) {
+                        Log.d(Constant.LOG_TAG, crawledView.toString());
+                        mItineraryPreviewList.add(crawledView);
+                    }
+                    //if position == 1 ("Present")
+                    if(position == 1) {
+                        if(endDate.after(todayDate)) {
+                            mItineraryPreviewList.add(crawledView);
+                        }
+                    }
+                    //if position == 2("Past")
+                    if(position == 2) {
+                        if(endDate.before(todayDate)) {
+                            mItineraryPreviewList.add(crawledView);
+                        }
+                    }
+
+                }
+                mItineraryPreviewAdapter = new ItineraryPreviewAdapter(rootView.getContext(), mItineraryPreviewList);
+                mRecyclerView.setAdapter(mItineraryPreviewAdapter);
+                if(mItineraryPreviewList.size() == 0) {
+                    mNoTripTextView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    mNoTripTextView.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/mad/madproject/fragments/MyTripFragment.java
+++ app/src/main/java/com/mad/madproject/fragments/MyTripFragment.java
@@ -3,7 +3,11 @@ package com.mad.madproject.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -37,7 +41,7 @@ import java.util.Locale;
 /**
  * My Trip Fragment handles displaying either all trip, past trip, or present trip, using spinner as the filter..
  */
-public class MyTripFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+public class MyTripFragment extends Fragment {
 
     private ArrayList<ItineraryPreview> mItineraryPreviewList = new ArrayList<>();
     private RecyclerView mRecyclerView;
@@ -50,90 +54,121 @@ public class MyTripFragment extends Fragment implements AdapterView.OnItemSelect
         final View rootView = inflater.inflate(R.layout.fragment_my_trip, container, false);
         Util.setFragmentToolbarTitle(this, "My Trips");
 
-        Spinner spinner = rootView.findViewById(R.id.spinner_filter_itinerary);
         mNoTripTextTv = (TextView) rootView.findViewById(R.id.text_no_itinerary);
 
-        //set the listener to the spinner.
-        spinner.setOnItemSelectedListener(this);
+        //set the view pager.
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.view_itinerary_history_recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+        //set the tabs layout
+        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.result_tabs);
+        //set up with the view pager.
+        tabLayout.setupWithViewPager(viewPager);
 
 
-        //all acts as the default value.
-        getItineraryPreviewData(rootView, "All");
+        //all acts as the default value. set initial.
+//        getItineraryPreviewData(rootView, "All");
 
         return rootView;
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        getItineraryPreviewData(view, parent.getItemAtPosition(pos).toString());
+    private void setupViewPager(ViewPager viewPager) {
+        FilterPagerAdapter adapter = new FilterPagerAdapter(getChildFragmentManager());
+
+        viewPager.setAdapter(adapter);
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    public class FilterPagerAdapter extends FragmentPagerAdapter {
 
-    }
+        public FilterPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-    /**
-     * Call database to get the data for the itinerary preview.
-     * @param rootView refer to the view to send the database file to
-     * @param status the spinner status.
-     */
-    private void getItineraryPreviewData(final View rootView, final String status) {
+        @Override
+        public Fragment getItem(int position) {
+            return FilterTripFragment.newInstance(position);
+        }
 
-        Util.getDatabaseReference("ItineraryPreview").orderByChild("ownerId").equalTo(Util.getUserUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                mItineraryPreviewList.clear();
-                for(DataSnapshot itineraryView: dataSnapshot.getChildren()) {
-                    Log.d(Constant.LOG_TAG, itineraryView.toString());
-                    ItineraryPreview crawledView = itineraryView.getValue(ItineraryPreview.class);
-                    Date endDate = new Date();
-                    Date todayDate = new Date();
+        @Override
+        public int getCount() {
+            //always 3, all, past, present.
+            return 3;
+        }
 
-                    try {
-                        Log.d("Try", crawledView.getEndDate());
-                        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-                        endDate = formatter.parse(crawledView.getEndDate());
-                    } catch (ParseException e) {
-                        Log.d("Try", "Failed");
-                        e.printStackTrace();
-                    }
-
-                    if(status.equals("All")) {
-                        Log.d(Constant.LOG_TAG, crawledView.toString());
-                        mItineraryPreviewList.add(crawledView);
-                    }
-                    if(status.equals("Past")) {
-                        if(endDate.before(todayDate)) {
-                            mItineraryPreviewList.add(crawledView);
-                        }
-                    }
-                    if(status.equals("Present")) {
-                        if(endDate.after(todayDate)) {
-                            mItineraryPreviewList.add(crawledView);
-                        }
-                    }
-
-                }
-                mItineraryPreviewAdapter = new ItineraryPreviewAdapter(rootView.getContext(), mItineraryPreviewList);
-                mRecyclerView.setAdapter(mItineraryPreviewAdapter);
-                if(mItineraryPreviewList.size() == 0) {
-                    mNoTripTextTv.setVisibility(View.VISIBLE);
-                }
-                else {
-                    mNoTripTextTv.setVisibility(View.INVISIBLE);
-                }
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "All";
+                case 1:
+                    return "Present";
+                case 2:
+                    return "Past";
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+            return null;
+        }
     }
+
+
+//    /**
+//     * Call database to get the data for the itinerary preview.
+//     * @param rootView refer to the view to send the database file to
+//     * @param status the spinner status.
+//     */
+//    private void getItineraryPreviewData(final View rootView, final String status) {
+//
+//        Util.getDatabaseReference("ItineraryPreview").orderByChild("ownerId").equalTo(Util.getUserUid()).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                mItineraryPreviewList.clear();
+//                for(DataSnapshot itineraryView: dataSnapshot.getChildren()) {
+//                    Log.d(Constant.LOG_TAG, itineraryView.toString());
+//                    ItineraryPreview crawledView = itineraryView.getValue(ItineraryPreview.class);
+//                    Date endDate = new Date();
+//                    Date todayDate = new Date();
+//
+//                    try {
+//                        Log.d("Try", crawledView.getEndDate());
+//                        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+//                        endDate = formatter.parse(crawledView.getEndDate());
+//                    } catch (ParseException e) {
+//                        Log.d("Try", "Failed");
+//                        e.printStackTrace();
+//                    }
+//
+//                    if(status.equals("All")) {
+//                        Log.d(Constant.LOG_TAG, crawledView.toString());
+//                        mItineraryPreviewList.add(crawledView);
+//                    }
+//                    if(status.equals("Past")) {
+//                        if(endDate.before(todayDate)) {
+//                            mItineraryPreviewList.add(crawledView);
+//                        }
+//                    }
+//                    if(status.equals("Present")) {
+//                        if(endDate.after(todayDate)) {
+//                            mItineraryPreviewList.add(crawledView);
+//                        }
+//                    }
+//
+//                }
+//                mItineraryPreviewAdapter = new ItineraryPreviewAdapter(rootView.getContext(), mItineraryPreviewList);
+//                mRecyclerView.setAdapter(mItineraryPreviewAdapter);
+//                if(mItineraryPreviewList.size() == 0) {
+//                    mNoTripTextTv.setVisibility(View.VISIBLE);
+//                }
+//                else {
+//                    mNoTripTextTv.setVisibility(View.INVISIBLE);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
 
 }
